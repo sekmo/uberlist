931ed35ba57f0efbfcc32d9b1e1425ef73ab9435
Francesco Mari <francescojjmari@gmail.com>
2017-10-08 13:36:25 +0200

    Add form error partial and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,5 @@
 module ApplicationHelper
-  def status_tag(boolean, options = {})
-    options[:true_text] ||= ""
-    options[:false_text] ||= ""
-    if boolean
-      content_tag(:span, options[:true_text], class: "status true")
-    else
-      content_tag(:span, options[:false_text], class: "status false")
-    end
-  end
-
-  def mark_complete_button(complete, task, redirect_to_project = {})
-    if complete
-      link_to("mark as incomplete", mark_as_incomplete_task_path(task), method: :post)
-    else
-      link_to("mark as complete", mark_as_complete_task_path(task), method: :post)
-    end
+  def error_messages_for(object)
+    render(partial: 'shared/error_messages', locals: {object: object})
   end
 end
